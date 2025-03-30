5ee10ca3f2e3729c9a1b0d69a40d35dbcdae99f6
nasi <n.alievaa00@gmail.com>
2025-03-31 02:11:46 +0700

    commit

---
+++ src/views/register.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="wrapper">
+    <BlockFormRegister/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BlockFormRegister from '@/components/blocks/block-form-register.vue'
+</script>
+
+<style scoped>
+
+</style>
